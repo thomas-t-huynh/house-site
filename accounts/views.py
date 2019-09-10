f1549ddb4fa8be8301acfccb5dabd4e2c702bb14
unknown <t_huynh927@yahoo.com>
2019-09-09 23:47:12 -0700

    added error message div and added a short js script to timeout the div

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        # register user
+        messages.error(request, 'Testing error message')
+        return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
 def login(request):
     if request.method == 'POST':
         # login user
+        return
     else:
         return render(request, 'accounts/login.html')
 
