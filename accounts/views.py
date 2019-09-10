7b66d93e126dfbba5eb5d67991c4f81993e4142e
unknown <t_huynh927@yahoo.com>
2019-09-09 22:47:37 -0700

    added login post request, setup up login/register pages

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+
+def register(request):
+    if request.method == 'POST':
+        # register user
+    else:
+        return render(request, 'accounts/register.html')
+
+def login(request):
+    if request.method == 'POST':
+        # login user
+    else:
+        return render(request, 'accounts/login.html')
+
+def logout(request):
+    return redirect('index')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
